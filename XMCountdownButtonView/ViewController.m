afa7154defaa9566280580a32b6b91f289ec091b
西门丨不吹雪 <505739041@qq.com>
2017-02-24 11:57:16 +0800

    test

--- XMCountdownButtonView/ViewController.m
+++ XMCountdownButtonView/ViewController.m
@@ -18,12 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    XMCountdownButton *button = [[XMCountdownButton alloc]initWithFrame:CGRectMake(100, 100, 100, 30) title:@"发送验证码" startTime:5 tapEvent:^{
+    //验证码按钮
+    XMCountdownButton *buttonOne = [[XMCountdownButton alloc]initWithFrame:CGRectMake(100, 100, 100, 30) title:@"发送验证码" startTime:5 tapEvent:^{
         NSLog(@"123");
     } callBack:^{
         NSLog(@"456");
     }];
-    [self.view addSubview:button];
+    [self.view addSubview:buttonOne];
+    
+    //抢购倒计时
+    XMCountdownButton *buttonTwo = [[XMCountdownButton alloc]initWithFrame:CGRectMake(20, 200, 200, 30) leftTime:24*60*60];
+    [self.view addSubview:buttonTwo];
+    buttonTwo.backgroundColor = [UIColor lightGrayColor];
 }
 
 @end
